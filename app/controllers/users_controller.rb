72b224b4aa012c81cec314a6ce9101011b6b3262
Daniel Huang <danielh714@gmail.com>
2015-12-20 22:44:14 -0800

    begin building out login route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 require 'json'
+require 'bcrypt'
 
 class UsersController < ApplicationController
-	def new
+	def create
 		p params
+		p '*' * 100
+		p params["user"]
+		User.create(email: params["user"]["email"], password: params["user"]["password"], name: params["user"]["name"])
+	end
+
+	def new #Login Route
+		if User.find_by(email: params["user"]["email"])
+			user = User.find_by()	
+		end
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+	validates :email, presence: true, uniqueness: true
+	validates :hash_password, presence: true
+
+	include BCrypt
+
+	def password
+	  @password ||= Password.new(hash_password)
+	end
+
+	def password=(new_password)
+	  @password = Password.create(new_password)
+	  self.hash_password = @password
+	end
 end
